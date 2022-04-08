7575241e124916cac3faa896a22ea590ce5dbdf1
Alexander Iran Chilel Casillas <casichi12@gmail.com>
2022-04-07 23:06:52 -0500

    Get countries and regions

--- server/Main.py
+++ server/Main.py
@@ -1,26 +1,47 @@
-import http.server
-import socketserver
-import json
-import pandas
-import sqlite3
-import time
+import http.server, socketserver, json, time, requests, pandas, sqlite3
 import random
 import os
 import hashlib
-import requests
 
 
 class ServiceHandler(http.server.SimpleHTTPRequestHandler):
-    # Get mothod
+    # Get method
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
-            self.send_header("Content-type", "text-json")
+            self.send_header('Content-type', 'text/json')
             self.end_headers()
+            # Get all countries
+            url = "https://restcountries.com/v3.1/all"
+            # Get by region
+            url_countries_by_region = "https://restcountries.com/v3.1/region/{region}"
 
-            #Get all countries
-            url = "https://rapidapi.com/apilayernet/api/rest-countries-v1"
+            headers = {
+                'x-rapidapi-key': "921cfc17abmsh42834139575656fp12725cjsn8ce3ad10333d",
+                'x-rapidapi-host': "restcountries-v1.p.rapidapi.com"
+            }
 
+            hash_languages = []
+            countries = []
+            times = []
+            regions_data = []
+            data = json.loads(requests.request("GET", url, headers=headers).text)
+            for information in data:
+                if information["region"] and not information["region"] in regions_data:
+                    regions_data.append(information["region"])
+            for region in regions_data:
+                start_time = time.time()
+                response_by_region = json.loads(
+                    requests.request("GET", url_countries_by_region.format(region=region), headers=headers).text
+                )
+                country_option = random.randint(0, len(response_by_region) - 1)
+                countries.append(response_by_region[country_option]['name']['common'])
+                key_language = list(response_by_region[country_option]['languages'].keys())[0]
+                hash_languages.append(hashlib.sha1(
+                    response_by_region[country_option]['languages'][str(key_language)].encode()).hexdigest())
+                end_time = time.time()
+                times.append(round((end_time - start_time) * 1000, 2))
+                print(countries)
 
 
 class ReuseAddrTCPServer(socketserver.TCPServer):
@@ -36,4 +57,3 @@ try:
 except:
     print("Closing the server")
     myserver.server_close()
-
